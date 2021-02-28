9edcf0bf3a686e13159cc143eee54ffd3017b7ce
Arti Mathanda <arti.mathanda@simplybusiness.co.uk>
2021-02-28 22:46:52 +0000

    Updates Button Component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,15 @@
-import React from "react";
-import styled, { css } from "styled-components";
-import PropTypes from "prop-types";
+import React from 'react';
+import styled, { css } from 'styled-components';
+import PropTypes from 'prop-types';
 
 const getVariant = (variant) => {
   console.log({ variant });
   switch (variant) {
-    case "primary":
+    case 'primary':
       return primary;
-    case "ghost":
+    case 'ghost':
       return ghost;
-    case "danger":
+    case 'danger':
       return danger;
     default:
       return primary;
@@ -18,11 +18,11 @@ const getVariant = (variant) => {
 const getSize = (size) => {
   console.log({ size });
   switch (size) {
-    case "small":
+    case 'small':
       return small;
-    case "medium":
+    case 'medium':
       return medium;
-    case "large":
+    case 'large':
       return large;
     default:
       return medium;
@@ -32,11 +32,9 @@ const getSize = (size) => {
 const primary = css`
   background: green;
 `;
-
 const ghost = css`
   background: grey;
 `;
-
 const danger = css`
   background: red;
 `;
@@ -45,19 +43,16 @@ const small = css`
   border-radius: 12px;
   padding: 10px 25px;
 `;
-
 const medium = css`
   font: 16px sans-serif;
   border-radius: 16px;
   padding: 10px 30px;
 `;
-
 const large = css`
   font: 26px sans-serif;
   border-radius: 26px;
   padding: 20px 35px;
 `;
-
 const buttonbase = css`
   color: white;
   border: none;
@@ -81,12 +76,13 @@ const Button = ({ variant, size, children }) => {
 export default Button;
 
 Button.propTypes = {
-  image: PropTypes.string,
-  size: PropTypes.oneOf(["medium", "large"]),
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  variant: PropTypes.oneOf(['primary', 'secondary', 'ghost']),
   children: PropTypes.element.isRequired,
 };
 
 Button.defaultProps = {
-  image: "primary",
-  size: "medium",
+  variant: 'primary',
+  size: 'medium',
+  children: 'Button Label',
 };
